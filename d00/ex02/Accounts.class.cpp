ab0c1b1e35b9f103afa9c9144afb4b33f986f295
Zachary Smith <z.smith32@gmail.com>
2017-07-03 23:44:06 -0700

    ex02, still need to work out the time stamp stuff

---
+++ d00/ex02/Accounts.class.cpp
@@ -0,0 +1,139 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pb.cpp                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/03 15:16:44 by zsmith            #+#    #+#             */
+/*   Updated: 2017/07/03 20:13:10 by zsmith           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.class.hpp"
+#include <iostream>
+#include <string>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) {
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+	this->_accountIndex = Account::_nbAccounts - 1;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+			<< ";amount:" << this->_amount
+			<< ";created"
+			<< std::endl;
+	return ;
+}
+
+Account::~Account() {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+			<< ";amount:" << this->_amount
+			<< ";closed"
+			<< std::endl;
+	return ;
+}
+
+int		Account::getNbAccounts(void) {
+	return (Account::_nbAccounts);
+}
+
+int		Account::getTotalAmount(void) {
+	return (Account::_totalAmount);
+}
+
+int		Account::getNbDeposits(void) {
+	return (Account::_totalNbDeposits);
+}
+
+int		Account::getNbWithdrawals(void) {
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void) {
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::_nbAccounts 
+				<< ";total:" << Account::_totalAmount
+				<< ";deposits:" << Account::_totalNbDeposits
+				<< ";withdrawals:" << Account::_totalNbWithdrawals
+				<< std::endl;
+}
+
+void	Account::makeDeposit(int deposit) {
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits++;
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+			<< ";p_amount:" << this->_amount - deposit
+			<< ";deposit:" << deposit
+			<< ";amount:" << this->_amount
+			<< ";nb_deposits:" << this->_nbDeposits
+			<< std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal) {
+	if (withdrawal <= this->_amount) {
+		this->_amount -= withdrawal;
+		this->_nbWithdrawals += 1;
+		Account::_totalAmount -= withdrawal;
+		Account::_totalNbWithdrawals++;
+	} else {
+		Account::_displayTimestamp();
+		std::cout << "index:" << this->_accountIndex
+			<< ";p_amount:" << this->_amount
+			<< ";withdrawal:"
+			<< "refused"
+			<< std::endl;
+		return (false);
+	}
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+			<< ";p_amount:" << this->_amount + withdrawal
+			<< ";withdrawal:" << withdrawal
+			<< ";amount:" << this->_amount
+			<< ";nb_withdrawals:" << this->_nbDeposits
+			<< std::endl;
+	return (true);
+}
+
+int		Account::checkAmount(void) const {
+	return(this->_amount);
+}
+
+void	Account::displayStatus(void) const {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+			<< ";amount:" << this->_amount
+			<< ";deposits:" << this->_nbDeposits
+			<< ";withdrawals:" << this->_nbWithdrawals
+			<< std::endl;
+}
+
+void	Account::_displayTimestamp(void) {
+	std::cout << "[20150406_153629] ";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
