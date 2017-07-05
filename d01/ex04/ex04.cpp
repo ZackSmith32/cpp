e47ded6db2c8870e56ea728b23b975e12b1afebc
Zachary Smith <z.smith32@gmail.com>
2017-07-05 15:20:44 -0700

    d01 : ex04

--- d01/ex04/ex04.cpp
+++ d01/ex04/ex04.cpp
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ZombieHorde.hpp                                    :+:      :+:    :+:   */
+/*   ex04.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/04 17:51:56 by zsmith            #+#    #+#             */
-/*   Updated: 2017/07/05 15:13:00 by zsmith           ###   ########.fr       */
+/*   Created: 2017/07/05 15:13:33 by zsmith            #+#    #+#             */
+/*   Updated: 2017/07/05 15:20:04 by zsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
 #include <iostream>
-#include <Zombie.hpp>
+#include <string>
 
-class ZombieHorde {
-public:
-	ZombieHorde(int n);
-	~ZombieHorde(void);
-	Zombie* horde;
-};
+int	main(void) {
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	ptr = &str;
+	std::string&	ref = str;
+
+	std::cout << "ptr : " << *ptr << std::endl;
+	std::cout << "ref : " << ref << std::endl;
+	return (0);
+}
