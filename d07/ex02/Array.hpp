10f170a5f688aa82eb6ef7f8e7a649c5c9aea035
Zachary Smith <z.smith32@gmail.com>
2017-07-14 10:01:03 -0700

    rearranged the header file to bring member function definitions outside of the class definition.

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -16,62 +16,74 @@
 #include <iostream>
 #include <string>
 
-template <class T = int>  							// wondering if this will work for default
+template <class T = int> 
 class Array {
 	
 	public:
-		Array() {
-			T		*newnew = new T[0];
-			
-			_size = 0;
-			_arr = newnew;
-		}
-
-		Array(unsigned int n) { 					// empty
-			T		*blank = new T[n];
-			
-			_size = n;
-			_arr = blank;
-		};
-
-		Array(Array const & src) {					// copy
-			*this = src;
-		};
-
+		Array();
+		Array(unsigned int n); 
+		Array(Array const & src);
 		Array(T & src);				
-		
-		~Array() {
-			std::cout << "deleting array" << std::endl;
-			delete [] _arr;
-		};
-
-		Array &	operator=(Array const & rhs) {		// assignment
-			int		i = 0;
-
-			if (this != &rhs) {
-				_arr = new T[rhs._size];
-				for ( ; i < rhs._size; i++) {
-					_arr[i] = rhs._arr[i];
-				}
-				_size = rhs._size;
-			}
-			return *this;
-		};
-
-		T &		operator[](int n) {
-			if (n > _size - 1)
-				throw std::exception();
-			return (_arr[n]);
-		}
-
-		int		size() const {
-			return (_size);
-		}
-
-
+		~Array();
+		Array &	operator=(Array const & rhs);
+		T &		operator[](int n);
+		int		size() const;
 	private:
 		int		_size;
 		T		*_arr;
 };
+
+template <class T> 
+Array<T>::Array(){
+	T		*newnew = new T[0];
+			
+	_size = 0;
+	_arr = newnew;
+}
+
+template <class T> 
+Array<T>::Array(unsigned int n) { 					// empty
+	T		*blank = new T[n];
+
+	_size = n;
+	_arr = blank;
+};
+
+template <class T> 
+Array<T>::Array(Array const & src) {					// copy
+	*this = src;
+};
+
+template <class T> 
+Array<T>::~Array() {
+	std::cout << "deleting array" << std::endl;
+	delete [] _arr;
+};
+
+template <class T> 
+Array<T> &	Array<T>::operator=(Array const & rhs) {		// assignment
+	int		i = 0;
+
+	if (this != &rhs) {
+		_arr = new T[rhs._size];
+		for ( ; i < rhs._size; i++) {
+			_arr[i] = rhs._arr[i];
+		}
+		_size = rhs._size;
+	}
+	return *this;
+};
+
+template <class T> 
+T &		Array<T>::operator[](int n) {
+	if (n > _size - 1)
+		throw std::exception();
+	return (_arr[n]);
+}
+
+template <class T> 
+int		Array<T>::size() const {
+	return (_size);
+}
 
 #endif
